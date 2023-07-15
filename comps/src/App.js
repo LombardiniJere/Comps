e64d4c9a23d2b42bbbfda83fa2d0ef715223c685
LombardiniJere <jere.lombardini@gmail.com>
2023-07-14 21:29:02 -0300

    adding buttons reusable

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,9 +1,37 @@
+import Buttons from "./components/Buttons/Buttons";
 
+import { GiAbdominalArmor, Gi3DHammer, GiBackPain, GiBattleAxe, GiBiceps } from "react-icons/gi";
 
 function App() {
+
+  const handleOnClick = () => {
+    console.log('Hello');
+  }
+
   return (
     <div className="App">
-
+      <Buttons onClick={ handleOnClick } primary rounded> 
+        < GiBiceps />
+        Primary !
+      </Buttons>
+      <Buttons secondary> 
+        < GiBattleAxe />
+        Secondary !
+      </Buttons>
+      <Buttons success>
+        < Gi3DHammer />
+        Success !
+      </Buttons>
+      <Buttons danger> 
+        < GiBackPain />
+        Danger !
+      </Buttons>
+      <Buttons warning>
+        < GiAbdominalArmor />
+        Warning !
+      </Buttons>
+      <Buttons rounded> Rounded !</Buttons>
+      <Buttons outline> Outline !</Buttons>
     </div>
   );
 }
---
+++ comps/src/components/Buttons/Buttons.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import className from 'classnames';
+
+function Buttons ({
+  children,
+  primary,
+  secondary,
+  success,
+  danger,
+  warning,
+  rounded,
+  outline,
+  ...rest
+}) {
+  const classes = className(
+    rest.className,
+    'flex items-center px-3 py-1.5 border', { 
+    'border-blue-500 bg-blue-500 text-white' : primary, // "primary: true"= if true style like 'mention'
+    'border-gray-900 bg-gray-900 text-white' : secondary,
+    'border-green-500 bg-green-500 text-white' : success,
+    'border-red-500 bg-red-500 text-white' : danger,
+    'border-yellow-400 bg-yellow-400 text-white' : warning,
+    'rounded-full' : rounded,
+    'bg-white text-' : outline
+  });
+
+  return (
+    <button { ...rest } className={ classes }>
+      { children }
+    </button>
+  )
+}
+
+
+
+export default Buttons
