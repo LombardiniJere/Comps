e901bc374d180955aef3ed0bbd395650622aead3
LombardiniJere <jere.lombardini@gmail.com>
2023-07-21 13:13:26 -0300

    creating modal

---
+++ comps/src/components/pages/ModalPage.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import Buttons from "../Buttons/Buttons";
+import Modal from "../Modal";
+
+
+function ModalPage() {
+  const [ showModal, setShowModa ] = useState(false);
+
+  const handleClick = () => {
+    setShowModa(true);
+  }
+  const handleCloseModal = () => {
+    setShowModa(false);
+  }
+
+  const actionBar = 
+    <div>
+      <Buttons primary onClick={ handleCloseModal }>Accept</Buttons>
+    </div>
+
+  const modal = 
+    <Modal onClose={ handleCloseModal } actionBar={ actionBar } >
+      <p>Imporat Agreement !</p>
+    </Modal>
+
+  return (
+    <div className="relative">
+      <Buttons primary onClick={ handleClick }>
+        ! Open Modal !
+      </Buttons>
+      {showModal && modal}
+    </div>
+  )
+}
+
+export default ModalPage;
--- comps/src/index.js
+++ comps/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-
+import { NavigationProvider } from './components/Context/Navigation';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <NavigationProvider>
+      <App />
+    </NavigationProvider>
 );
+
 
